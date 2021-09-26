cf8849e826f9752aa8182bb505356ffc10d1514c
Ronnie Myers <hi@ronniemyers.com>
2021-09-25 19:08:37 -0500

    test

--- solution.py
+++ solution.py
@@ -1,53 +1,58 @@
-# Assignment 2
+# import socket module
 from socket import *
+# In order to terminate the program
 import sys
-from email.parser import BytesParser
-import pprint
 
-HOST = "127.0.0.1"
-PORT = 13331
 
-def webServer(port=PORT):
+def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    serverSocket.bind((HOST, PORT))
-    serverSocket.listen(1)
+    # Prepare a server socket
+    serverSocket.bind(("", port))
+    # Fill in start
+
+    # Fill in end
 
     while True:
-        connectionSocket, addr = serverSocket.accept()
+        # Establish the connection
+        # print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept() # Fill in start      #Fill in end
         try:
-            message = connectionSocket.recv(1024)
-            _, headers = message.split(b'\r\n', 1)
-            header_bytes = BytesParser().parsebytes(headers)
-            headers = dict(header_bytes.items())
-            filename = message.split()[1]
-            # print('File requested: %s\n' % filename.decode())
-            # print('Request Headers:\n')
-            pprint.pprint(headers, width=200)
-            f = open(filename[1:])
-            outputdata = f.read()
-            res = 'HTTP/3 200 OK\r\n'
-            res += 'Content-Type: text/html\n'
-            res += '\n'
-            connectionSocket.send(res.encode())
 
-            for i in range(0, len(outputdata)):
-                connectionSocket.send(outputdata[i].encode())
-            connectionSocket.send('\r\n'.encode())
-            response_message = '\nSuccess: HTTP/3 200 OK\n'
-            # print(response_message)
-            serverSocket.close()
-        except IOError:
-            response_message = '\nFailure: HTTP/3 404 NOT FOUND\n'
-            connectionSocket.send('HTTP/3 404 NOT FOUND'.encode())
-            # print(response_message)
-            serverSocket.close()
+            try:
+                message =  connectionSocket.recv(1024)
+                filename = message.split()[1]
+                f = open(filename[1:])
+                outputdata = f.read()
+
+                # Send one HTTP header line into socket.
+                # Fill in start
+
+                # Fill in end
+
+                # Send the content of the requested file to the client
+                for i in range(0, len(outputdata)):
+                    connectionSocket.send(outputdata[i].encode())
+
+                connectionSocket.send("\r\n".encode())
+                connectionSocket.close()
+            except IOError:
+                connectionSocket.send("HTTP/2 404 NOT FOUND".encode())
+        # Send response message for file not found (404)
+        # Fill in start
+
+        # Fill in end
+
+        # Close client socket
+        # Fill in start
+
+        # Fill in end
 
         except (ConnectionResetError, BrokenPipeError):
             pass
 
-        serverSocket.close()
-        sys.exit()
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
+
 
 if __name__ == "__main__":
     webServer(13331)
